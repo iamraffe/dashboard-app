635df76949302b34c70e4671a70078efc9b0cad3
Rafa Ramírez <raffe90@gmail.com>
2016-05-10 03:45:54 +0200

    Tutorial completed?

--- app/assets/javascripts/chatrooms.js
+++ app/assets/javascripts/chatrooms.js
@@ -1,2 +1,13 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).on('turbolinks:load', function() {
+  submitNewMessage();
+});
+
+function submitNewMessage(){
+  $('textarea#message_content').keydown(function(event) {
+    if (event.keyCode == 13) {
+        $('[data-send="message"]').click();
+        $('[data-textarea="message"]').val(" ")
+        return false;
+     }
+  });
+}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510011019) do
+ActiveRecord::Schema.define(version: 20160510013919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160510011019) do
     t.string   "topic"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
   end
 
   create_table "messages", force: :cascade do |t|
