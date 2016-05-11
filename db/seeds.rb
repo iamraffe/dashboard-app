4917ee91cf07272471b0366a2f5c22d89308ed39
raffe90 <raffe90@gmail.com>
2016-05-11 18:18:31 +0200

    Adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,21 @@
 100.times do |i|
   u = User.create({
         first_name: Faker::Name.first_name,
+        username: Faker::Lorem.word,
         last_name: Faker::Name.last_name,
         email: Faker::Internet.email,
         password: "mysecretpassword",
         password_confirmation: "mysecretpassword",
-        phone_number: Faker::PhoneNumber.phone_number,
-        avatar: "https://randomuser.me/api/portraits/thumb/men/#{i}.jpg",
-        # care_teams: c
       })
-
-  u.care_teams << [c, ct].sample
-
-  u.add_role :patient
 end
 
 100.times do |i|
   u = User.create({
         first_name: Faker::Name.first_name,
+        username: Faker::Lorem.word,
         last_name: Faker::Name.last_name,
         email: Faker::Internet.email,
         password: "mysecretpassword",
         password_confirmation: "mysecretpassword",
-        phone_number: Faker::PhoneNumber.phone_number,
-        gender: "F",
-        diabetes: [true, false].sample,
-        heart_disease: [true, false].sample,
-        date_of_birth: Faker::Time.between(50.years.ago, 5.years.ago),
-        avatar: "https://randomuser.me/api/portraits/thumb/women/#{i}.jpg",
-        # care_teams: c
       })
-
-  u.care_teams << [c, ct].sample
-
-  u.add_role :patient
 end
